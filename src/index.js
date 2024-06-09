f6e0e82f4e03ac1c8d1eedacb5867d659ad24213
snkt-30 <sanketkhonde30@gmail.com>
2024-06-09 06:19:31 +0530

    Added roles and user_roles throught tables

--- src/index.js
+++ src/index.js
@@ -6,21 +6,37 @@ const app = express();
 
 // const { User } = require("./models/index");
 // const serviceRepository = require("./services/user-service");
+//
 
-const { PORT,JWT_KEY } = require("./config/serverConfig");
+// const db = require('./models/index');
+
+const { User, Role } = require("./models/index");
+
+const { PORT, JWT_KEY } = require("./config/serverConfig");
 
 function setupAndStartServer() {
-  
   app.listen(PORT, async () => {
-
     // middlewares
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({ extended: true }));
 
+    if (process.env.SYNC_DB) {
+      db.sequelize.sync({ alter: true });
+    }
+
+    const user = await User.findByPk(1);
+    const role = await Role.findByPk(1);
+    user.addRole(role);
+    // console.log(user);
+    // const response = await user.getRoles();
+    const response = await role.getUsers();
+
+    console.log(response);
+
     app.use("/api", apiRoutes);
 
     // const serviceRepo = new serviceRepository();
-                                                                              
+
     // const token = serviceRepo.createToken({email:'Wedding@gmail.com',id:'3'});
     // console.log(token);
     // const reponse = serviceRepo.verifyToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6IldlZGRpbmdAZ21haWwuY29tIiwiaWQiOiIzIiwiaWF0IjoxNzE3MDU3NzkwLCJleHAiOjE3MTcwNjEzOTB9.gCnpzGc9WVrzlTFooGLGr1VGzFx5ZyinmdQoepbVk4E');
